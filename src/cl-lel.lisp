2d448913fef2c5ff90056f320c81468d6c352c0a
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2021-10-29 23:39:59 +0200

    Implement lazy-seq + operators; Test some basic stuff

--- src/cl-lel.lisp
+++ src/cl-lel.lisp
@@ -1,3 +1,85 @@
 ;;;; cl-lel.lisp
 
+(defpackage #:cl-lel
+  (:use #:cl)
+  (:export #:delay
+	   #:force
+	   #:lcons
+	   #:lcons-car
+	   #:lcons-cdr
+	   #:lazy-take
+	   #:lazy-take-while
+	   #:lazy-drop
+	   #:lazy-drop-while
+	   #:lazy-nth
+	   #:lazy-filter
+	   #:list->lazy-seq
+	   #:numbers-from
+	   #:cycle))
+
 (in-package #:cl-lel)
+
+
+(defmacro delay (expression)
+  `(lambda () ,expression ))
+
+(defun force (delayed-expression)
+  (when delayed-expression
+    (funcall delayed-expression)))
+
+(defmacro lcons (head tail)
+  `(cons (delay ,head) (delay ,tail)))
+
+(defun lcons-car (lcons)
+  (force (car lcons)))
+
+(defun lcons-cdr (lcons)
+  (force (cdr lcons)))
+
+(defun lazy-take (n lcons)
+  (loop :repeat n
+	:for elem = (lcons-car lcons)
+	:while elem
+	:collect elem
+	:do (setf lcons (lcons-cdr lcons))))
+
+(defun lazy-take-while (predicate lcons)
+  (loop :for elem = (lcons-car lcons)
+	:while (and elem (funcall predicate elem))
+	:collect (lcons-car lcons)
+	:do (setf lcons (lcons-cdr lcons))))
+
+(defun lazy-drop (n lcons)
+  (if (zerop n)
+      lcons
+      (lazy-drop (1- n) (lcons-cdr lcons))))
+
+(defun lazy-drop-while (predicate lcons)
+  (if (funcall predicate (lcons-car lcons))
+      (lazy-drop-while predicate (lcons-cdr lcons))
+      lcons))
+
+(defun lazy-nth (n lcons)
+  (lcons-car (lazy-drop n lcons)))
+
+(defun lazy-filter (predicate lcons)
+  (when lcons
+    (let ((elem (lcons-car lcons)))
+      (if (funcall predicate elem)
+	  (lcons elem (lazy-filter predicate (lcons-cdr lcons)))
+	  (lazy-filter predicate (lcons-cdr lcons))))))
+
+(defun list->lazy-seq (list)
+  (when list
+    (lcons (car list) (list->lazy-seq (cdr list)))))
+
+(defun numbers-from (start &optional (step-size 1))
+  (lcons start (numbers-from (+ start step-size))))
+
+(defun cycle (sequence)
+  (let ((seq-length (length sequence)))
+    (labels ((inner-cycle (sequence index)
+	       (lcons (nth index sequence) (inner-cycle sequence (mod (1+ index) seq-length)))))
+      (inner-cycle sequence 0))))
+
+
---
+++ t/tests.lisp
@@ -0,0 +1,57 @@
+(defpackage #:cl-lel-tests
+  (:use #:cl #:cl-lel #:fiveam)
+  (:export #:all-tests))
+
+(in-package #:cl-lel-tests)
+
+(def-suite all-tests)
+
+(in-suite all-tests)
+
+(test delay-force
+  (is (functionp (delay 123)))
+  (is (= 0 (force (delay 0)))))
+
+(test lcons
+  (let ((lcons (lcons 0 1)))
+    (is (= 0 (lcons-car lcons)))
+    (is (= 1 (lcons-cdr lcons)))))
+
+(test lazy-take
+  (let ((lazy-seq (numbers-from 1)))
+    (is (equal '(1 2 3) (lazy-take 3 lazy-seq)))))
+
+(test lazy-take-while
+  (let ((lazy-seq (numbers-from 1))
+	(predicate (lambda (x) (<= x 3))))
+    (is (equal '(1 2 3)
+	       (lazy-take-while predicate lazy-seq)))))
+
+(test lazy-drop
+  (let ((lazy-seq (numbers-from 1)))
+    (is (equal '(4 5 6)
+	       (lazy-take 3 (lazy-drop 3 lazy-seq))))))
+
+(test lazy-drop-while
+  (let ((lazy-seq (numbers-from 1))
+	(predicate (lambda (x) (<= x 3))))
+    (is (equal '(4 5 6)
+	       (lazy-take 3 (lazy-drop-while predicate lazy-seq))))))
+
+(test lazy-nth
+  (let ((lazy-seq (numbers-from 1)))
+    (is (= 4 (lazy-nth 3 lazy-seq)))))
+
+(test lazy-filter
+  (is (equal '(2 4 6) (lazy-take 3 (lazy-filter #'evenp
+						(numbers-from 1))))))
+
+(test list->lazy-seq
+  (let ((l '(1 2 3)))
+    (is (equal l (lazy-take 3 (list->lazy-seq l))))
+    (is (null (lazy-drop 3 (list->lazy-seq l))))
+    (is (null (lazy-nth 10 (list->lazy-seq l))))
+    (is (equal '(2) (lazy-take 10 (lazy-filter #'evenp
+					       (list->lazy-seq l)))))))
+
+
